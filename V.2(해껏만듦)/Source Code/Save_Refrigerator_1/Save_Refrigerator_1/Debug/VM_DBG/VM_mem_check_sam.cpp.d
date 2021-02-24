c3b22317b6ad2a363bb8a95d2a14cee25c70356c
GENYF <gnyf0915@gmail.com>
2021-02-24 16:27:13 +0900

    Delete Visual Studio File

--- V.2(해껏만듦)/Source Code/Save_Refrigerator_1/Save_Refrigerator_1/Debug/VM_DBG/VM_mem_check_sam.cpp.d
+++
@@ -1,2 +0,0 @@
-C:\Users\syb91\AppData\Local\Temp\VMBuilds\Save_Refrigerator_1\esp8266_nodemcuv2\Debug\VM_DBG\VM_mem_check_sam.cpp.o: \
- C:\Program\ Files\ (x86)\Microsoft\ Visual\ Studio\ 14.0\Common7\IDE\Extensions\2dbtayvr.ylu\Micro\ Platforms\default\debuggers\VM_DBG\VM_mem_check_sam.cpp
